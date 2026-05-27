aa7c01b730a1ab371e30cb12b57f8e8ab4b8a6f6
Victor <victornpefgem@gmail.com>
2026-05-27 16:35:27 -0300

    Organizing the course structure

---
+++ 01-getting-started/condicionals/pay-calculator-2.0.py
@@ -0,0 +1,10 @@
+hrs = float(input("Enter Hours: "))
+rate = float(input("Enter Hourly Rate: "))
+
+if hrs <= 40:
+    pay = hrs * rate
+    print("Your Total Pay is: US$", pay)
+else:
+    pay = 40 * rate
+    tpay = ((hrs - 40) * (rate * 1.5)) + pay 
+    print("Your Total Pay with Overtime: US$", tpay)

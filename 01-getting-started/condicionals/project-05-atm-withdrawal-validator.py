5a32ebec2cf656d8b8ef9f1a271ca430772ad123
Victor <victornpefgem@gmail.com>
2026-05-28 17:10:24 -0300

    condicionals

--- 01-getting-started/condicionals/project-05-atm-withdrawal-validator.py
+++ 01-getting-started/condicionals/project-05-atm-withdrawal-validator.py
@@ -8,12 +8,13 @@ except :
 
 min_amount = 10
 max_amount = 500
+multiple = 10
 
 if withdraw < min_amount :
     print(f"Error, Minimum Withdrawal Amount is US${min_amount}")
 elif withdraw > max_amount :
     print(f"Error, Maximum Withdrawal Amount is US${max_amount}")
-elif withdraw % 10  != 0 :
-    print("Error, Withdrawal Amount must be a Multiple of US$10")
+elif withdraw % multiple  != 0 :
+    print(f"Error, Withdrawal Amount must be a Multiple of US${multiple}")
 else :
     print(f"Withdrawal Approved, Please Take Your Cash US${withdraw}")
